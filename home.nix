65819a6703acb1dad17e4f92127136ac0a3d0140
JohnFodero <jtfodero@gmail.com>
2025-04-24 10:35:01 -0500

    adds a bunch of new plugins, some work, some dont... sigh...

--- home.nix
+++ home.nix
@@ -22,12 +22,14 @@
     tree
     htop
     just
-
+    
+    # containers
     qemu
     minikube 
 
+    # apple-specific
     apple-sdk
-    catppuccin
+    
     # source control
     graphite-cli
     gh
@@ -38,7 +40,6 @@
 #    cargo
 #    rustc
 	
-#    docker-buildx
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
@@ -145,14 +146,114 @@
     opts = {
       relativenumber = true;
       showmode = false;
-
+      tabstop = 2;
+      shiftwidth = 2;
       ignorecase = true;
       smartcase = true;
 
     };
-    colorschemes.catppuccin.enable = true;
+    keymaps = [
+    	#### telescope ####
+        {
+          action = "<cmd>Telescope find_files<CR>";
+          key = "<leader>ff";
+        }
+        {
+          action = "<cmd>Telescope live_grep<CR>";
+          key = "<leader>fg";
+        }
+        {
+          action = "<cmd>Telescope buffers<CR>";
+          key = "<leader>fb";
+        }
+        {
+          action = "<cmd>Telescope help_tags<CR>";
+          key = "<leader>fh";
+        }
+				{
+					action = "<cmd>lua vim.lsp.buf.hover()<cr>";
+					key = "<leader>K";
+				}
+				{
+					action = "<cmd>lua vim.lsp.buf.definition()<cr>";
+					key = "<leader>gd";
+				}
+				{
+					action = "<cmd>lua vim.lsp.buf.declaration()<cr>";
+					key = "<leader>gD";
+				}
+				{
+					action = "<cmd>lua vim.lsp.buf.implementation()<cr>";
+					key = "<leader>gi";
+				}
+				{
+					action = "<cmd>lua vim.lsp.buf.type_definition()<cr>";
+					key = "<leader>go";
+				}
+				{
+					action = "<cmd>lua vim.lsp.buf.references()<cr>";
+					key = "<leader>gr";
+				}
+				{
+					action = "<cmd>lua vim.lsp.buf.signature_help()<cr>";
+					key = "<leader>gs";
+				}
+				{
+					action = "<cmd>lua vim.lsp.buf.rename()<cr>";
+					key = "<leader>rn";
+				}
+				{
+				  action = "<cmd>lua vim.lsp.buf.format({async = true})<cr>";
+				# action = "<cmd>lua conform.format({lsp_fallback=true, async=true, timeout_ms=1000})<cr>";
+					key = "<leader>fm";
+				}
+				{
+					action = "<cmd>lua vim.lsp.buf.code_action()<cr>";
+					key = "<leader>ga";
+				}
+				#### HARPOON ####
+				# does not work yet...
+				{
+					action.__raw = "function() require'harpoon':list():add() end";
+					key = "<leader>a";
+					mode = "n";
+				}
+				{
+					action.__raw = "function() require'harpoon'.ui:toggle_quick_menu(require'harpoon':list()) end";
+					key = "<C-e>";
+					mode = "n";
+				}
+				{
+					action.__raw = "function() require'harpoon':list():select(1) end";
+					key = "<C-h>";
+					mode = "n";
+				}
+				{
+					action.__raw = "function() require'harpoon':list():select(2) end";
+					key = "<C-j>";
+					mode = "n";
+				}
+    ];
 
-    plugins.copilot-vim.enable = true;
+    colorschemes.catppuccin.enable = true;
+	
+    plugins.telescope.enable = true;
+		plugins.harpoon = {
+			enable = true;
+			enableTelescope = true;
+		};
+
+    plugins.treesitter = {
+          enable = true;
+          folding = false;
+          settings.indent.enable = true;
+        };
+    plugins.web-devicons.enable = true;
+    plugins.which-key = {
+          enable = true;
+          settings.preset = "helix";
+        };
+    plugins.copilot-vim.enable = false;
     plugins.copilot-chat.enable = true;
     plugins.lualine.enable = true;
     
@@ -169,16 +270,66 @@
         };
       };
     };
+	plugins.startify = {
+    enable = true;
 
+    settings = {
+      custom_header = [
+        ""
+        "     ███╗   ██╗██╗██╗  ██╗██╗   ██╗██╗███╗   ███╗"
+        "     ████╗  ██║██║╚██╗██╔╝██║   ██║██║████╗ ████║"
+        "     ██╔██╗ ██║██║ ╚███╔╝ ██║   ██║██║██╔████╔██║"
+        "     ██║╚██╗██║██║ ██╔██╗ ╚██╗ ██╔╝██║██║╚██╔╝██║"
+        "     ██║ ╚████║██║██╔╝ ██╗ ╚████╔╝ ██║██║ ╚═╝ ██║"
+        "     ╚═╝  ╚═══╝╚═╝╚═╝  ╚═╝  ╚═══╝  ╚═╝╚═╝     ╚═╝"
+      ];
+
+      # When opening a file or bookmark, change to its directory.
+      change_to_dir = false;
+
+      # By default, the fortune header uses ASCII characters, because they work for everyone.
+      # If you set this option to 1 and your 'encoding' is "utf-8", Unicode box-drawing characters will
+      # be used instead.
+      use_unicode = true;
+
+      lists = [ { type = "dir"; } ];
+      files_number = 30;
+
+      skiplist = [
+        "flake.lock"
+      ];
+    };
+  };
+    plugins.conform-nvim = {
+      enable = true;
+      settings = {
+        formatters_by_ft = {
+          css = [ "prettier" ];
+          html = [ "prettier" ];
+          json = [ "prettier" ];
+          lua = [ "stylua" ];
+          markdown = [ "prettier" ];
+          nix = [ "alejandra" ];
+          python = [ "black" ];
+          ruby = [ "rubyfmt" ];
+          terraform = [ "tofu_fmt" ];
+          tf = [ "tofu_fmt" ];
+          yaml = [ "yamlfmt" ];
+        };
+				format_on_save = {
+        	lsp_fallback = true;
+          async = false;
+          timeout_ms = 500;
+        };
+      };
+    };
     plugins.lsp = {
       enable = true;
       servers = {
       	dockerls.enable = true;
 	gopls.enable = true;
         nil_ls.enable = true;
-
         ruff.enable = true;
-	
 	rust_analyzer = {
 	  enable = true;
 	  installCargo = false;
